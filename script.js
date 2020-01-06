adb041bc88640ce3737be145b8eec007c00c9088
abickford <atb5498@gmail.com>
2020-01-06 16:19:54 -0600

    initial javascript

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-na
+var urlBrew = "https://api.openbrewerydb.org/breweries?by_city=austin";
+
+$.ajax({
+    url: urlBrew,
+    method: "GET"
+}).then(function (responseBrew) {
+    console.log(responseBrew);
+});
+
+var myurl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=by-pinthouse&location=austin";
+
+$.ajax({
+    url: myurl,
+    headers: {
+        'Authorization': 'Bearer Tj1ORfVUyCEhKkIIHsCm6CLztz_Z7fMnITBAKUNYLVZivHuV-4wQ41Me9lSI9eyhAbwSIuMqerfrTWaB7FY4TQIYy1zs_1i8l1ueMUrirIccE_ZWosspqnwoGp8TXnYx',
+    },
+    method: 'GET',
+    dataType: 'json',
+    success: function (data) {
+        console.log('success: ' + JSON.stringify(data));
+    }
+});
