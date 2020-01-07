80fbd92895d083dbb1a0ad3f5f053f25bdf934fc
abickford <atb5498@gmail.com>
2020-01-07 14:08:51 -0600

    created inital card for brewery search results

--- js/script.js
+++ js/script.js
@@ -3,8 +3,9 @@ var searchBtn = $(".button");
 searchBtn.on("click", function (event) {
     event.preventDefault();
     $("#results").empty();
-    var city = $(".input").val();
-    var urlBrew = "https://api.openbrewerydb.org/breweries?by_city=" + city;
+    var city = $("#city").val();
+    var state = $("#state").val();
+    var urlBrew = "https://api.openbrewerydb.org/breweries?by_city=" + city + "&by_state=" + state;
 
     $.ajax({
         url: urlBrew,
@@ -12,40 +13,50 @@ searchBtn.on("click", function (event) {
     }).then(function (responseBrew) {
         console.log(responseBrew);
         for (var i = 0; i < responseBrew.length; i++) {
-            var breweryName = responseBrew[i].name;
-            var results = $("<h2>").append(breweryName)
-            $("#results").append(results);
+            var brewName = responseBrew[i].name;
+            var brewPhone = responseBrew[i].phone;
+            var brewResults = $("#results");
+
+            var columns = $("<div>");
+            columns.addClass("columns");
+            columns.append(brewResults);
+            var column = $("<div>");
+            column.addClass("column");
+            column.append(columns);
+
+            // var results = $("<h2>").append(brewName)
+            // $("#results").append(results);
         }
     });
 });
 
-var searchURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=by-pinthouse&location=austin";
+// var searchURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=by-pinthouse&location=austin";
 
-$.ajax({
-    url: searchURL,
-    headers: {
-        'Authorization': 'Bearer Tj1ORfVUyCEhKkIIHsCm6CLztz_Z7fMnITBAKUNYLVZivHuV-4wQ41Me9lSI9eyhAbwSIuMqerfrTWaB7FY4TQIYy1zs_1i8l1ueMUrirIccE_ZWosspqnwoGp8TXnYx',
-    },
-    method: 'GET',
-    dataType: 'json',
-    success: function (data) {
-        console.log(JSON.stringify(data));
-    }
-});
+// $.ajax({
+//     url: searchURL,
+//     headers: {
+//         'Authorization': 'Bearer Tj1ORfVUyCEhKkIIHsCm6CLztz_Z7fMnITBAKUNYLVZivHuV-4wQ41Me9lSI9eyhAbwSIuMqerfrTWaB7FY4TQIYy1zs_1i8l1ueMUrirIccE_ZWosspqnwoGp8TXnYx',
+//     },
+//     method: 'GET',
+//     dataType: 'json',
+//     success: function (data) {
+//         console.log(JSON.stringify(data));
+//     }
+// });
 
-var reviewsURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/j3CWjH0XtesbbDZFVq3c7Q/reviews"
+// var reviewsURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/j3CWjH0XtesbbDZFVq3c7Q/reviews"
 
-$.ajax({
-    url: reviewsURL,
-    headers: {
-        'Authorization': 'Bearer Tj1ORfVUyCEhKkIIHsCm6CLztz_Z7fMnITBAKUNYLVZivHuV-4wQ41Me9lSI9eyhAbwSIuMqerfrTWaB7FY4TQIYy1zs_1i8l1ueMUrirIccE_ZWosspqnwoGp8TXnYx',
-    },
-    method: 'GET',
-    dataType: 'json',
-    success: function (data) {
-        console.log(JSON.stringify(data.reviews));
-    }
-});
+// $.ajax({
+//     url: reviewsURL,
+//     headers: {
+//         'Authorization': 'Bearer Tj1ORfVUyCEhKkIIHsCm6CLztz_Z7fMnITBAKUNYLVZivHuV-4wQ41Me9lSI9eyhAbwSIuMqerfrTWaB7FY4TQIYy1zs_1i8l1ueMUrirIccE_ZWosspqnwoGp8TXnYx',
+//     },
+//     method: 'GET',
+//     dataType: 'json',
+//     success: function (data) {
+//         console.log(JSON.stringify(data.reviews));
+//     }
+// });
 
 // --------------------------------------------------------------------------------------------------------playing with geolocation and leaflet------------------------------------------------------------------------------
 
