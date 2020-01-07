1b5ec73f54b26d66fd9904a0f9218a76e307d2bc
abickford <atb5498@gmail.com>
2020-01-07 11:00:19 -0600

    created results

--- js/script.js
+++ js/script.js
@@ -1,22 +1,48 @@
-var urlBrew = "https://api.openbrewerydb.org/breweries?by_city=austin";
+var searchBtn = $(".button");
 
-$.ajax({
-    url: urlBrew,
-    method: "GET"
-}).then(function (responseBrew) {
-    console.log(responseBrew);
+searchBtn.on("click", function (event) {
+    event.preventDefault();
+    $("#results").empty();
+    var city = $(".input").val();
+    var urlBrew = "https://api.openbrewerydb.org/breweries?by_city=" + city;
+
+    $.ajax({
+        url: urlBrew,
+        method: "GET"
+    }).then(function (responseBrew) {
+        console.log(responseBrew);
+        for (var i = 0; i < responseBrew.length; i++) {
+            var breweryName = responseBrew[i].name;
+            var results = $("<h2>").append(breweryName)
+            $("#results").append(results);
+        }
+    });
 });
 
-var myurl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=by-pinthouse&location=austin";
+var searchURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=by-pinthouse&location=austin";
 
 $.ajax({
-    url: myurl,
+    url: searchURL,
     headers: {
         'Authorization': 'Bearer Tj1ORfVUyCEhKkIIHsCm6CLztz_Z7fMnITBAKUNYLVZivHuV-4wQ41Me9lSI9eyhAbwSIuMqerfrTWaB7FY4TQIYy1zs_1i8l1ueMUrirIccE_ZWosspqnwoGp8TXnYx',
     },
     method: 'GET',
     dataType: 'json',
     success: function (data) {
-        console.log('success: ' + JSON.stringify(data));
+        console.log(JSON.stringify(data));
+    }
+});
+
+var reviewsURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/j3CWjH0XtesbbDZFVq3c7Q/reviews"
+
+$.ajax({
+    url: reviewsURL,
+    headers: {
+        'Authorization': 'Bearer Tj1ORfVUyCEhKkIIHsCm6CLztz_Z7fMnITBAKUNYLVZivHuV-4wQ41Me9lSI9eyhAbwSIuMqerfrTWaB7FY4TQIYy1zs_1i8l1ueMUrirIccE_ZWosspqnwoGp8TXnYx',
+    },
+    method: 'GET',
+    dataType: 'json',
+    success: function (data) {
+        console.log(JSON.stringify(data.reviews));
     }
 });
