7c5a895016718d480b23da3b54923946f2151c5b
abickford <atb5498@gmail.com>
2020-01-08 16:15:52 -0600

    tried to figure out the yelp api

--- js/script.js
+++ js/script.js
@@ -7,6 +7,24 @@ searchBtn.on("click", function (event) {
     var state = $("#state").val();
     var brewURL = "https://api.openbrewerydb.org/breweries?by_city=" + city + "&by_state=" + state;
 
+    function searchByPhone(searchUrl) {
+        console.log(window.performance);
+        console.log("run");
+        $.ajax({
+            url: searchUrl,
+            headers: {
+                'Authorization': 'Bearer Tj1ORfVUyCEhKkIIHsCm6CLztz_Z7fMnITBAKUNYLVZivHuV-4wQ41Me9lSI9eyhAbwSIuMqerfrTWaB7FY4TQIYy1zs_1i8l1ueMUrirIccE_ZWosspqnwoGp8TXnYx',
+            },
+            method: 'GET',
+            dataType: 'json',
+            success: function (data) {
+                console.log(data);
+            }
+        });
+    }
+
+    var throttledSearchByPhone = _.throttle(searchByPhone, 300)
+
     $.ajax({
         url: brewURL,
         method: "GET"
@@ -15,70 +33,47 @@ searchBtn.on("click", function (event) {
         for (var i = 0; i < responseBrew.length; i++) {
             var brewName = responseBrew[i].name;
             var brewWeb = responseBrew[i].website_url;
-            var brewPhone = responseBrew[i].phone;
-            var brewLat = responseBrew[i].latitude;
+            let brewPhone = parseInt(responseBrew[i].phone);
+            console.log(brewPhone);
             var brewLon = responseBrew[i].longitude;
+            var brewLat = responseBrew[i].latitude;
 
-            var brewResults = $("#results");
-            var column = $("<div>").addClass("column is-one-quarter");
-            var card = $("<div>").addClass("card");
-            var cardImage = $("<div>").addClass("card-image");
-            var brewImage = $("<figure>").addClass("image is-4by3");
-            var imageSrc = $("<img>");
-            imageSrc.attr("src", "https://bulma.io/images/placeholders/1280x960.png");
-            var cardContent = $("<div>").addClass("card-content");
-            var media = $("<div>").addClass("media");
-            var mediaContent = $("<div>").addClass("media-content");
-            var title = $("<P>").addClass("title is-4");
-            title.text(brewName);
-            var subTitle = $("<p>").addClass("subtitle is-6");
-            subTitle.text(brewWeb);
-            var subTitle2 = $("<p>").addClass("subtitle is-6");
-            subTitle2.text(brewPhone);
+            if ((brewLon !== null) && (brewLat !== null) && (brewPhone !== NaN)) {
+                var brewResults = $("#results");
+                var column = $("<div>").addClass("column is-one-quarter");
+                var card = $("<div>").addClass("card");
+                var cardImage = $("<div>").addClass("card-image");
+                var brewImage = $("<figure>").addClass("image is-4by3");
+                var imageSrc = $("<img>");
+                imageSrc.attr("src", "https://bulma.io/images/placeholders/1280x960.png");
+                var cardContent = $("<div>").addClass("card-content");
+                var media = $("<div>").addClass("media");
+                var mediaContent = $("<div>").addClass("media-content");
+                var title = $("<p>").addClass("title is-4");
+                title.text(brewName);
+                var subTitle = $("<p>").addClass("subtitle is-6");
+                subTitle.text(brewWeb);
+                var subTitle2 = $("<p>").addClass("subtitle is-6");
+                subTitle2.text(brewPhone);
 
-            mediaContent.append(title);
-            mediaContent.append(subTitle);
-            mediaContent.append(subTitle2);
-            media.append(mediaContent);
-            cardContent.append(media);
-            brewImage.append(imageSrc);
-            cardImage.append(brewImage);
-            card.append(cardImage);
-            card.append(cardContent);
-            column.append(card);
-            brewResults.append(column);
+                mediaContent.append(title);
+                mediaContent.append(subTitle);
+                mediaContent.append(subTitle2);
+                media.append(mediaContent);
+                cardContent.append(media);
+                brewImage.append(imageSrc);
+                cardImage.append(brewImage);
+                card.append(cardImage);
+                card.append(cardContent);
+                column.append(card);
+                brewResults.append(column);
 
-            console.log(brewName);
+                let searchURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search/phone?phone=+1" + brewPhone;
 
-            var searchURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?latitude=" + brewLat + "&longitude=" + brewLon;
-
-            $.ajax({
-                url: searchURL,
-                headers: {
-                    'Authorization': 'Bearer Tj1ORfVUyCEhKkIIHsCm6CLztz_Z7fMnITBAKUNYLVZivHuV-4wQ41Me9lSI9eyhAbwSIuMqerfrTWaB7FY4TQIYy1zs_1i8l1ueMUrirIccE_ZWosspqnwoGp8TXnYx',
-                },
-                method: 'GET',
-                dataType: 'json',
-                success: function (data) {
-                    console.log(JSON.stringify(data));
-                }
-            });
+                throttledSearchByPhone(searchURL);
+            }
         }
     });
-
-    // var reviewsURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/j3CWjH0XtesbbDZFVq3c7Q/reviews"
-
-    // $.ajax({
-    //     url: reviewsURL,
-    //     headers: {
-    //         'Authorization': 'Bearer Tj1ORfVUyCEhKkIIHsCm6CLztz_Z7fMnITBAKUNYLVZivHuV-4wQ41Me9lSI9eyhAbwSIuMqerfrTWaB7FY4TQIYy1zs_1i8l1ueMUrirIccE_ZWosspqnwoGp8TXnYx',
-    //     },
-    //     method: 'GET',
-    //     dataType: 'json',
-    //     success: function (data) {
-    //         console.log(JSON.stringify(data.reviews));
-    //     }
-    // });
 });
 
 // --------------------------------------------------------------------------------------------------------playing with geolocation and leaflet------------------------------------------------------------------------------
@@ -124,3 +119,5 @@ var beerBtn = $("#beer-btn");
 beerBtn.on("click", function () {
     getLocation();
 });
+
+
